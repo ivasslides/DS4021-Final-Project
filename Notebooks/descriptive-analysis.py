3efa04d462d716a3c96cb638e2538c5146eefe61
Iliana Vasslides <ilianamvass@gmail.com>
2025-12-09 16:47:40 -0500

    adding descriptive file

--- Notebooks/descriptive-analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# print out mean and standard deviation for all variables - see "data-description.py" for exisiting code 
-
-# scatterplot comparing all variables to target variable 
-
-# distribution (histogram) of 1 and 2 for fetal health 
-
