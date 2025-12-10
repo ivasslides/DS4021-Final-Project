7be9bd3893c0455b0732f1ad501a7af248eb16da
Iliana Vasslides <ilianamvass@gmail.com>
2025-12-10 09:39:17 -0500

    descriptive work

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,33 +1,38 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
-# loading csv file into dataframe
+# Loading csv file into dataframe
 df = pd.read_csv('./Data/fetal_health.csv')
 
-# only keeping relevant columns 
+# Only keeping relevant columns 
 col_keeping = ['baseline value', 'accelerations', 'fetal_movement', 'uterine_contractions', 'light_decelerations', 'severe_decelerations',
        'prolongued_decelerations', 'abnormal_short_term_variability', 'mean_value_of_short_term_variability',
        'percentage_of_time_with_abnormal_long_term_variability', 'mean_value_of_long_term_variability', 'fetal_health']
 df = df[col_keeping]
 print(df.columns) 
 
-# replacing any 'fetal_heath' of 3 to 2
-# we are only have two classification groups to make the models simpler
+# Replacing any 'fetal_heath' of 3 to 2
+# We are only have two classification groups to make the models simpler
 df['fetal_health'] = df['fetal_health'].replace(3.0, 2.0)
 
-# verify that the replacement worked
+# Verify that the replacement worked
 print("Fetal_health unique values:", df['fetal_health'].unique())
 
-# save cleaned df to csv file 
+# Renaming `baseline value` to `baseline_value` for consistency
+df.rename(columns={'baseline value': 'baseline_value'}, inplace=True)
+# Verifying the column rename
+print("Columns after renaming:", df.columns)
+
+# Save cleaned df to csv file 
 df.to_csv('./Data/cleaned_fetal_health.csv', index=False)
 
-# train test splitting the data 
+# Train test splitting the data 
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
-# saving training set to csv
+# Saving training set to csv
 train_df.to_csv("./Data/train_set.csv", index=False)
 
-# saving test set to csv
+# Saving test set to csv
 test_df.to_csv("./Data/test_set.csv", index=False)
 
 print("Training and test sets saved as csv files.") 
